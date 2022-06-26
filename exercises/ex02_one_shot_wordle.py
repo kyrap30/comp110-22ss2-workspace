c5beb4e6494066241ddb669be1fd3ef702e787b1
Kyra Pollock <kpollock@unc.edu>
2022-06-26 19:23:21 -0400

    try

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,22 +1,24 @@
-"""EX02 - One shot wordle."""
 __author__ = "730466380"
+"""EX02 - One shot wordle."""
 
 WORD: str = "python"
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-i: int = 0
+
+input: int = 0
 box_emoji: str = ""
 f: int = 0
 
-word_guess: str = str(input(f"What is your {len(WORD)} guess? "))
+word_guess: str = input(f"What is your 6-letter guess? ")
 
 if len(WORD) != len(word_guess):
-    word_guess: str = input("That was not 6 letters! Try again: ")
+    word_guess: str = str(input("That was not 6 letters! Try again: "))
 
 
-while i < len(word_guess):
-    if WORD[i] == word_guess[i]:
+while input < len(word_guess):
+    if WORD[input] == word_guess[input]:
         print(GREEN_BOX)
     else: 
         typed: bool = False
@@ -28,7 +30,7 @@ while i < len(word_guess):
             print(WHITE_BOX)
         else:
             print(YELLOW_BOX)
-    i += 1
+    input += 1
 
 if word_guess == "python":
     print("Woo! You got it")
