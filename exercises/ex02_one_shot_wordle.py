eb27ac2f43dc176350dd3974c12e0146a21108e0
Kyra Pollock <kpollock@unc.edu>
2022-06-26 20:43:49 -0400

    ok

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,22 +5,22 @@ WORD: str = "python"
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-actplace: int = 0
-place: int = 0
+
 box_emoji: str = ""
 
-word_guess: str = str(input(f"What is your {len(WORD)} guess? "))
-
+word_guess: str = str(input(f"What is your 6-letter guess? "))
 while len(WORD) != len(word_guess):
     word_guess: str = str(input("That was not 6 letters! Try again: "))
 
+place: int = 0
 
 while place < len(word_guess):
-
     if WORD[place] == word_guess[place]:
-        print(GREEN_BOX)
+        box_emoji += GREEN_BOX
+
     else: 
         typed: bool = False
+        actplace: int = 0
 
         while typed is not True and actplace < len(WORD):
             if WORD[actplace] == word_guess[typed]:
@@ -28,19 +28,16 @@ while place < len(word_guess):
             actplace += 1
 
         if typed is not True:
-            print(WHITE_BOX)
+            box_emoji += WHITE_BOX 
+
         else:
-            print(YELLOW_BOX)
+            box_emoji += YELLOW_BOX
 
     place += 1
 
 if word_guess == "python":
     print("Woo! You got it")
     print(box_emoji)
-    exit()
 else: 
     print("Not quite. Play again soon!")
     print(box_emoji)
-    exit()
-
-
