034db66892d30c2fa8512e6e0f4b17cbbb49bee8
Kyra Pollock <kpollock@unc.edu>
2022-07-03 13:49:27 -0400

    d

--- lessons/LS14.py
+++ lessons/LS14.py
@@ -3,6 +3,7 @@
 names: list[str] = ["Kris", "Kaki", "Ezri","'Marc"]
 
 # Example of iterating through names using a while loop
+print("while output:")
 i: int = 0
 while i < len(names):
     name: str = names[i]
