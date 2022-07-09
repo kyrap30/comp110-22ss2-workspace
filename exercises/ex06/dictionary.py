8e7773c8b9ab5774b00837a3f2d527e4cb0887a1
Kyra Pollock <kpollock@unc.edu>
2022-07-08 22:38:41 -0400

    yay

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -1,19 +1,34 @@
 """Functions that are being tested."""
 __author__ = "730466380"
 
-def invert()
-    things: dict[str,str] 
-    return v:k for k,v in things.items()}
+
+def invert(things: dict[str, str]) -> dict[str, str]:
+    """Inverts keys anmd values of dictionary."""
+    inv_things = {}
+    for key in things:
+        value = things[key]
+        if value in inv_things:
+            raise KeyError("Already exists.")
+        inv_things[value] = key
+    return inv_things
 
 
-def favorite_color(color_list):
-    color_list = list(color_list())
-    print(color_list)
-    x = max(color_list, key = color_list.count)
-    return x
+def favorite_color(color_dict: dict[str, str]) -> str:
+    """Finds most popular color."""
+    output = {}
+    for key in color_dict:
+        if color_dict[key] in output:
+            output[color_dict[key]] += 1
+        else:
+            output[color_dict[key]] = 1
+    highest = max(output.values())
+    for key in color_dict:
+        if output[color_dict[key]] == highest:
+            return color_dict[key]
 
 
-def count(num_list: list[str]) -> dict[str,int]:
+def count(num_list: list[str]) -> dict[str, int]:
+    """Finds the word with the highest frequency."""
     frequency = {}
     for i in num_list:
         if i in frequency.keys():
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -1,10 +1,104 @@
 """Testing the functions within in dictionary."""
 __author__ = "730466380"
 
-from exercises.ex05.dictionary import invert
-from exercises.ex05.dictionary import favorite_color
-from exercises.ex05.dictionary import count
-
+from exercises.ex06.dictionary import invert
+from exercises.ex06.dictionary import favorite_color
+from exercises.ex06.dictionary import count
 import pytest
 
-def 
+
+def test_invert_nothing() -> None:
+    """Tests for nothing in the dict."""
+    things: dict[str, str] = {}
+    assert invert(things) == {}
+
+
+def test_invert_one_item() -> None:
+    """Asserts that with one pair, they switch spots."""
+    things: dict[str, str] = {'kyra': 'pollock'}
+    assert invert(things) == {'pollock': 'kyra'}
+
+
+def test_invert_multiple_items() -> None:
+    """Tests for when there are multiple of the same value."""
+    things: dict[str, str] = {
+        'pizza': 'party',
+        'surprise': 'yay',
+        'fun': 'pencil'
+    }
+    expected_inv: dict[str, str] = {
+        'party': 'pizza',
+        'yay': 'surprise',
+        'pencil': 'fun'
+    }
+    assert invert(things) == expected_inv
+
+
+def test_invert_error() -> None:
+    """Tests to make sure the KeyError is being raised."""
+    with pytest.raises(KeyError):
+        things: dict[str, str] = {
+            'pizza': 'party',
+            'surprise': 'party'
+        }
+        invert(things)
+
+
+def test_favorite_color_one() -> None:
+    """Tests for output with one entry."""
+    color_dict: dict[str, str] = {'kyra': 'green'}
+    expected: str = 'green'
+    assert favorite_color(color_dict) == expected
+
+
+def test_favorite_color_tie() -> None:
+    """Tests favorite color is starts with a letter higher in alphabet if tied with another."""
+    color_dict: dict[str, str] = {
+        'candace': 'green',
+        'perry': 'blue',
+        'ferb': 'blue',
+        'phineas': 'green'
+    }
+    expected: str = 'green'
+    assert favorite_color(color_dict) == expected
+
+
+def test_favorite_tied() -> None:
+    """Tests the output when there is one of each color."""
+    color_dict: dict[str, str] = {
+        'perry': 'pink',
+        'ferb': 'blue',
+        'candace': 'black',
+        'phineas': 'green'
+    }
+    expected: str = 'pink'
+    assert favorite_color(color_dict) == expected
+
+
+def test_count_nothing() -> None:
+    """Testing for an empty list."""
+    num_list: list[str] = {}
+    expected_frequency: dict[str, str] = {}
+    assert count(num_list) == expected_frequency
+
+
+def test_count_same() -> None:
+    """Testing output if there are the two different words are in the list the same amount."""
+    num_list: list[str] = ["pizza", "ranch", "tired", "pizza", "ranch"]
+    expected_output: dict[str, int] = {
+        "pizza": 2,
+        "ranch": 2,
+        "tired": 1
+    }
+    assert count(num_list) == expected_output
+
+
+def test_count_normal() -> None:
+    """Testing a list with normal values."""
+    num_list: list[str] = ["time", "time", "time", "count", "count", "tired"]
+    expected_output: dict[str, int] = {
+        "time": 3,
+        "count": 2,
+        "tired": 1
+    }
+    assert count(num_list) == expected_output
